66295debc986c19c24b822d458fb02112fd1da1e
Lee Ji Yun <jylee22.96@gmail.com>
2023-01-31 16:29:37 +0900

    [추가] 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './assets/scss/App.scss';
+// component
+import PortfolioPage from './page/PortfolioPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          임시 테스트 합니다.
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<PortfolioPage/>}/>
+      </Routes>
     </div>
   );
 }
