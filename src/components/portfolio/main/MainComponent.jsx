7eab7fc90e8654891d85dce1bce6db84c2731654
DESKTOP-8N3GG4O\GGG <jylee22.96@gmail.com>
2023-02-01 18:48:18 +0900

    [수정] 메인 파트 수정

--- src/components/portfolio/main/MainComponent.jsx
+++ src/components/portfolio/main/MainComponent.jsx
@@ -10,11 +10,11 @@ const MainComponent = () => {
           <div className="text">
             <h1>LEE JI YUN PORTFOLIO.</h1>
             <p>UI/UX 디자이너 & 웹 퍼블리셔에서 <h2>웹 개발자</h2> 길로 고요히 걸어가다.</p>
-          </div>
+            </div>
           <div className="scroll"></div>
 
-          <MainImage />
         </div>
+        <div className="bg"></div>
       </div>
     </div>
   );
